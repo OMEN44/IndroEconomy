32596aa7887784a5088927a29d397d34907ab826
OMEN44 <hswal2@eq.edu.au>
2021-10-30 18:04:48 +1000

    Finished the PlayerMine class

--- src/main/java/omen44/omens_economy/events/PlayerMine.java
+++ src/main/java/omen44/omens_economy/events/PlayerMine.java
@@ -12,6 +12,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class PlayerMine implements Listener {
 
@@ -23,13 +24,23 @@ public class PlayerMine implements Listener {
     @EventHandler
     public void onPlayerMine(BlockBreakEvent event) {
         Player player = event.getPlayer();
-        Block block = event.getBlock();
+        String block = event.getBlock().getType().toString();
+        String symbol = config.getString("moneySymbol");
 
         List<String> blocks = new ArrayList<>(config.getConfigurationSection("blocks").getKeys(false));
 
-        if (blocks.contains(block.getType().toString()) &&
+        if (blocks.contains(block) &&
                 !(player.getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH))) {
-            main.economyUtils.setWallet(player, 1);
+
+            List<String> drops = new ArrayList<>(config.getStringList("blocks." + block));
+            player.sendMessage(drops.toString());
+
+            Random random = new Random();
+            String drop = drops.get(random.nextInt(drops.size()));
+            int amount = main.economyUtils.getMoney(player, "Wallet") + Integer.parseInt(drop);
+
+            player.sendMessage("you got " + symbol + drop);
+            main.economyUtils.setWallet(player, amount);
         }
 
     }
